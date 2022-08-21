6d73df4b903e48abb0f7c25bb9e91a9e06accba2
fractalbach <fractalbach@gmail.com>
2022-08-21 01:31:40 -0700

    added a faster and cleaner version

--- generate-parentheses/gen.py
+++ generate-parentheses/gen.py
@@ -30,16 +30,54 @@ def backtrack(cur, loc, out):
 		backtrack(cur-1, list.copy(loc), out)
 
 
-
 def output(loc, out):
 	s = [')'] * (2*len(loc))
 	for v in loc:
 		s[v] = '('
 	out += [''.join(s)]
 
+
+# ====================================================================
+#    Faster and Cleaner Version
+# ____________________________________________________________________
+
+def generateParenthesis(n):
+	out = []
+
+	def solve(s, open, close, depth):
+		nonlocal out
+		nonlocal n
+
+		tabs = '| '*depth
+		print(f"{tabs}CALL '{s}', {open}, {close}")
+		
+		depth += 1
+		
+		if len(s) == n*2:
+			out += [s]
+			print(f"{tabs}OUTPUT {s}")
+			# print(f"{tabs}RETURN")
+			return
+			
+		if open < n:
+			solve(s+'(', open+1, close, depth)
+			
+		if close < open:
+			solve(s+')', open, close+1, depth)
+		
+		# print(f"{tabs}END")
+		
+			
+	solve('', 0, 0, 0)
+	return out
+
+			
+# ====================================================================
+		
 	
 if __name__ == '__main__':
-	arr = generate(4)
+	# arr = generate(4)
+	arr = generateParenthesis(4)
 	for i, x in enumerate(arr):
 		print(f'{i:3}: {x}',)
 		
